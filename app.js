8143778fd8b54cfa0a47235b5aa30e7a5acec856
Kristina <kristinashevchenko03@gmail.com>
2018-03-31 14:00:01 +0300

    made with express

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const express = require("express");
 let app = express();
 app.use(express.static('public'));
-const server = http.createServer((req, res) => {
-
-});
-
-server.listen('3000', () => {
+app.listen('3000', () => {
     console.log('Server is running');
 });
